28961b206fb051516c03851f47adb5bac9fc9cff
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-06 10:47:05 -0300

    tela incial, add monitorarmento com js para saber qunado clicou e oq que foi escrito no campo de input

---
+++ js/home.js
@@ -0,0 +1,24 @@
+
+// link para criar sala 
+const criarSala = document.querySelector("#criar-sala")
+
+criarSala.addEventListener("click", function () {
+    console.log("nhaa")
+
+    location.href= "../donoDaSala.html"
+})
+
+// link para criar player
+
+const player = document.querySelector("#entrar-sala")
+
+player.addEventListener("click", function () {
+    location.href= "../jogador.html"
+})
+
+// monitorar o que tem dentro do campo 
+const link = document.querySelector("#link")
+
+link.addEventListener("input", function () {
+    console.log(this.value)
+})
