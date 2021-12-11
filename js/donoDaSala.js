b3df1f4a65618c00868ebcbdf980e14086d6b188
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-11 19:09:22 -0300

    chave na tela

--- js/donoDaSala.js
+++ js/donoDaSala.js
@@ -1,3 +1,6 @@
+let url = "http://127.0.0.1:5500/donoDaSala.html"
+
+
 const sala = fetch("https://ffgames134.herokuapp.com/createRoom/")
     .then(function (sala)
     {
@@ -7,12 +10,15 @@ const sala = fetch("https://ffgames134.herokuapp.com/createRoom/")
     .then(function (sala)
     {
         
-        let link = location.hash + '/' + sala.keyRoom
-        document.getElementById("link").innerHTML = link;
+        let link =  sala.keyRoom
+        
         console.log(link)
+
+        let newUrl = url + link 
+        document.getElementById("link").innerHTML = newUrl;
        
     })
 
 // let url = new URL()
 
-
+console.log (new URL ("http://127.0.0.1:5500/donoDaSala.html"+ link))
