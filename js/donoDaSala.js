db10cb7d200abb046121a7399a9e83a72ec83f6d
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-11 17:37:45 -0300

    pegar dados JSON

--- js/donoDaSala.js
+++ js/donoDaSala.js
@@ -1,1 +1,18 @@
-get 
+const sala = fetch("https://ffgames134.herokuapp.com/createRoom/")
+    .then(function (sala)
+    {
+        return sala.json()
+    })
+
+    .then(function (sala)
+    {
+        
+        let link = location.hash + '/' + sala.keyRoom
+        document.getElementById("link").innerHTML = link;
+        console.log(link)
+       
+    })
+
+// let url = new URL()
+
+
