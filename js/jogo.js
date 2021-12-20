f38b75191c764afcc5d5314a0fb2755d93bc820f
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-20 16:32:55 -0300

    criando elementos com DOM

--- js/jogo.js
+++ js/jogo.js
@@ -1,5 +1,6 @@
-const sessao = document.querySelectorAll('#lista') 
-    console.log(sessao)
-function criarTabela() {
-    
-}
+const secao = document.querySelector("#lista") 
+var h3 = document.createElement("h3")
+var texto = document.createTextNode("Um título qualquer");
+texto.className = 'teste';
+
+secao.appendChild(texto)
