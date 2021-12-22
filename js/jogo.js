4e867d107a13e78630d3990a98cc856763c294c8
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-22 15:12:55 -0300

    Update jogo.js

--- js/jogo.js
+++ js/jogo.js
@@ -1,37 +1,58 @@
 
 function dadosJogador() {
 
-    var dados = {
-        img: "roxo",
-        nick: "Diogo",
-        valor: 300,
-        
-    }
-
-
-
-  
-
 
     const sessaojogador = document.createElement("aside")
     const idImg = document.createElement("img")
     const nickH3 = document.createElement("h3")
     const ValorP = document.createElement("p")
-    
-    idImg.textContent = dados.img
-    nickH3.textContent = dados.nick
-    ValorP.textContent = dados.valor
+
 
 
     sessaojogador.appendChild(idImg)
     sessaojogador.appendChild(nickH3)
     sessaojogador.appendChild(ValorP)
 
+    idImg.textContent = dado.img
+    nickH3.textContent = dado.nick
+    ValorP.textContent = dado.valor
 
     const section = document.querySelector("#lista")
-    
+
     section.appendChild(sessaojogador)
 
 
 }
 window.onload = dadosJogador
+
+
+var dado = [{
+    img: "roxo",
+    nick: "diogo",
+    valor: 400
+
+},
+{
+    img: "roxo",
+    nick: "diogo",
+    valor: 400
+
+},
+{
+    img: "roxo",
+    nick: "Diogo",
+    valor: 300,
+
+},
+{
+    img: "roxo",
+    nick: "Diogo",
+    valor: 300,
+
+}
+]
+
+dado.forEach(function (jogador) {
+    dadosJogador(jogador)
+
+});
