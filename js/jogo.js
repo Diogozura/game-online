de07bf5eb2f549b109547428d2fee5c8e1120c7a
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-22 14:38:51 -0300

    add array com dados

--- js/jogo.js
+++ js/jogo.js
@@ -1,6 +1,37 @@
-const secao = document.querySelector("#lista") 
-var h3 = document.createElement("h3")
-var texto = document.createTextNode("Um título qualquer");
-texto.className = 'teste';
 
-secao.appendChild(texto)
+function dadosJogador() {
+
+    var dados = {
+        img: "roxo",
+        nick: "Diogo",
+        valor: 300,
+        
+    }
+
+
+
+  
+
+
+    const sessaojogador = document.createElement("aside")
+    const idImg = document.createElement("img")
+    const nickH3 = document.createElement("h3")
+    const ValorP = document.createElement("p")
+    
+    idImg.textContent = dados.img
+    nickH3.textContent = dados.nick
+    ValorP.textContent = dados.valor
+
+
+    sessaojogador.appendChild(idImg)
+    sessaojogador.appendChild(nickH3)
+    sessaojogador.appendChild(ValorP)
+
+
+    const section = document.querySelector("#lista")
+    
+    section.appendChild(sessaojogador)
+
+
+}
+window.onload = dadosJogador
