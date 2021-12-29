f8ce60a9c6de7787d0ca7098461faa7fe7728599
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-29 07:26:09 -0300

    iniciar o post

--- js/criarSala.js
+++ js/criarSala.js
@@ -37,8 +37,8 @@ function dadosDaSala(form) {
 }
 
 function iniciaGame() {
-  
-  alert("parabens deu tudo certo")
+ 
+ alert(`olá `)
 }
 
 function validaDados(jogador) {
@@ -49,6 +49,7 @@ function validaDados(jogador) {
   if (jogador.nome.length == " ") {
     erro.push("falta o nome")
   }
+  escolhido()
   
   return erro
 }
@@ -57,7 +58,39 @@ function validaDados(jogador) {
 
 fetch()
 
+// pegar link
+// -linkSala.sala()
+//     .then(sala => {
+//         sala.keyRoom
+//         let link = document.querySelector('#pwd_spn')
+//       link.textContent = sala.keyRoom
+//       return link
+//     })
 
+// copiar 
 
+// -document.getElementById("cp_btn").addEventListener("click", copy_password);
+
+// function copy_password() {
+//     var copyText = document.getElementById("pwd_spn");
+//     var textArea = document.createElement("textarea");
+//     textArea.value = copyText.textContent;
+//     document.body.appendChild(textArea);
+//     textArea.select();
+//     document.execCommand("Copy");
+//     textArea.remove();
+
+// validar identificador 
+function escolhido() {
+  var res = '';
+  const items = document.querySelector('.cores');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].checked) {
+      res = items[i].value
+      break;
+    }
+  }
+  return res;
+}
 
 console.error( "espertinho, tá querendo ver oq seu cabra safado... vai procurar oq fazer, olhar o código dos outros é feio 😒");
