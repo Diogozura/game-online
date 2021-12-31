541366abb0cbbbea6a64bdf3d4c7dfe50f6fba23
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-12-30 23:56:57 -0300

    consegui pegar dados da API e mostrar na tela game.html

--- js/jogo.js
+++ js/jogo.js
@@ -1,8 +1,8 @@
 import { dadosSala } from "./dadosSala.js"
 
-const dadosJogador = document.querySelector("#lista")
-function addNaTela(jogador) {
 
+function addNaTela(jogador) {
+    const dadosJogador = document.querySelector("#lista")
     const jogadorAside = montaCampo(jogador)
     dadosJogador.appendChild(jogadorAside)
 
@@ -23,14 +23,14 @@ function montaCampo() {
 function montaImg() {
     const idImg = document.createElement("img")
     idImg.classList.add("img-jogador")
-    idImg.textContent = dados.img
+    idImg.textContent = "bonito "
 
     return idImg
 }
 function montaNome() {
     const nickH3 = document.createElement("h3")
     nickH3.classList.add("nome-jogador")
-    nickH3.textContent = dados.nick
+    nickH3.textContent = "cu"
 
     return nickH3
 }
@@ -42,10 +42,21 @@ function montaValor() {
 
     return ValorP
 }
+const id = document.querySelector("#cor")
+const nome = document.querySelector("#nome")
+const saldo = document.querySelector("#saldo")
 
 dadosSala.dados()
     .then(dados => {
-        var nome = dados.players 
-        console.log(typeof nome)
+        const dadoJogador = dados.players
+        dadoJogador.forEach(element => {
+            console.log(element.identificador, element.namePlayer, element.saldo)
+            id.textContent = element.identificador;
+            nome.textContent = element.namePlayer;
+            saldo.textContent = "R$" + " " + element.saldo
+        });
+        
+        const str = JSON.stringify(dados.players)
+        console.log(str)
         
     })
