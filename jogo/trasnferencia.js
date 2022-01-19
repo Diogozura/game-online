068eab0a699f6cfdfb463353d576b9d0a4a12593
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-01-19 10:02:33 -0300

    trasferencia

--- jogo/trasnferencia.js
+++ jogo/trasnferencia.js
@@ -1,75 +1,21 @@
+const tranferir = document.querySelector("[trasferir]")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const transeferir = document.querySelector('[trasferir]')
-transeferir.addEventListener("submit", (event) => {
+tranferir.addEventListener("submit", (event) => {
   event.preventDefault()
-  const valor = event.target.querySelector("[valor]").value
-
-
-  alert("ainnnn me clicou seu safado" + valor)
-
-  document.getElementById("myForm").style.display = "none";
-
+  validaValor()
 })
 
-const btnTransfe = document.querySelectorAll("[btn-transfere]")
 
-//   .addEventListener("click", (event) => {
-//     event.preventDefault()
-//     openForm()
-//     console.log("sai de mim desgraça")
-// })
+const validaValor = () => {
+  const valor = document.querySelector("[valor]").value
 
-for (i = 0; i < btnTransfe.length; i++) {
-  btnTransfe[i].addEventListener("click", () => {
-    openForm()
-   tela()
-  })
+  if (!valor >= 1) {
+      const erroValor = document.querySelector("[erro-valor]")
+      erroValor.className = "hide"
+      console.log("valor inválido")
+      return false
+  } else {
+      return valor
+  }
+
 }
-
-
-
-
-function tela() {
-  console.log(btnTransfe)
-  var btn = document.querySelector("[btn-transfere]");
-  var h3 = btn.parentElement;
-  console.log(h3)
-}
-
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-  
-}
-
-const fechaForm = document.querySelector("[close]")
-fechaForm.addEventListener("click", (event) => {
-  event.preventDefault()
-  document.getElementById("myForm").style.display = "none";
-})
-
