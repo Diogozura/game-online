32515efedd6f363f9a6aa1d042c5a8dff142c3ed
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-03-04 20:36:00 -0300

    validação ok

--- js/home.js
+++ js/home.js
@@ -10,10 +10,12 @@ const criarPlayer = document.querySelector("[iniciar-player]")
 
 criarPlayer.addEventListener("click", function (event) {
     event.preventDefault()
-    const token = document.querySelector("[url-player]").value
+    const token = document.querySelector("[codigo-sala]").value
+    const erro = document.getElementById("erro")
+
     const idTem = localStorage.getItem("idPlayer")
     localStorage.setItem("token", token)
-    
+
     const dados = () => {
         return fetch(`https://ffgames134.herokuapp.com/dadosSala/?keyRoom=${token}`)
             .then(resposta => {
@@ -22,24 +24,30 @@ criarPlayer.addEventListener("click", function (event) {
 
                 // validando dados direto que vem e aprovando , caso ele tenha as informçãoes de dadosSala, vai ser redirecionado para pagina do game
 
-                if (resposta.status == 200 && idTem != null) {
+                if (resposta.status != 200) {
+                    erro.style.display = "block"
+                }
+                else {
                     console.log("Passou")
-                    // externo 
-                    window.location.href = '../game-online/jogo/game.html'
 
-                    // local
-                    // window.location.href = '../jogo/game.html'
-                    
+                    // externo 
+                    // window.location.href = '../game-online/criarJogador/jogador.html'
+
+                    // local 
+                    window.location.href = '../criarJogador/jogador.html'
+
+
+
                     // return true
                 }
                 // caso ele não tenha o id , vou ter certeza que ele ainda não registrou um criarPlayer
-                else {
+                if (resposta.status == 200 && idTem != null) {
                     console.log("você não passou")
                     // externo 
-                    window.location.href = '../game-online/criarJogador/jogador.html'
+                    // window.location.href = '../game-online/jogo/game.html'
 
-                    // local 
-                    // window.location.href = '../criarJogador/jogador.html'
+                    // local
+                    window.location.href = '../jogo/game.html'
 
                 }
                 return resposta.json()
@@ -53,7 +61,7 @@ criarPlayer.addEventListener("click", function (event) {
     console.log("clicou home")
 
     dados()
-       
+
 
 
 })
