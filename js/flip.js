f6eee5c5b9ff4957e84a62e54654470e2e78428a
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-03-08 22:19:06 -0300

    flip card
    flip card , agora todos funciona , só falta o estilo certo

--- js/flip.js
+++ js/flip.js
@@ -1,6 +1,6 @@
 function flip(event){
 	var element = event.currentTarget;
-	if (element.className === "card") {
+	if (element.className === "carde") {
     if(element.style.transform == "rotateY(180deg)") {
       element.style.transform = "rotateY(0deg)";
     }
