5a46cb6efa93e9db925eaa2b3424a4563680df3d
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-03-09 22:11:00 -0300

    ajustes no criar player

--- criarJogador/jogador.js
+++ criarJogador/jogador.js
@@ -6,7 +6,7 @@ dadosSala.temCor()
   .then(temCor => {
     const dadoJogador = temCor.coresRestante
     console.log(dadoJogador)
-    dadoJogador.forEach(function(cor)  {
+    dadoJogador.forEach(function (cor) {
       criaAside(cor.identificador)
       // criarLabelCor(cor.identificador)
     });
@@ -21,7 +21,7 @@ function criaAside(cor) {
   const input = criaInputCor(cor)
   const label = criarLabelCor(cor)
 
-  
+
   aside.appendChild(label)
   aside.appendChild(input)
 
@@ -30,15 +30,16 @@ function criaAside(cor) {
   bloco.appendChild(aside)
 
   return aside
-  }
-  
+}
+
 
 
 function criaInputCor(cor) {
   const input = document.createElement("input")
-  input.type = "radio" 
+  input.type = "radio"
   input.value = cor
-  input.name= "cores"
+  input.name = "cores"
+  input.id = cor
 
   return input
 }
@@ -47,19 +48,21 @@ function criaInputCor(cor) {
 
 function criarLabelCor(cor) {
   const svg = document.createElement("IMG")
-svg.src =  `../img/avatar/${cor}.svg`
-svg.classList.add("icone")
-      
+  svg.src = `../img/avatar/${cor}.svg`
+  svg.classList.add("icone")
+
   // ` <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor"
   // class="bi bi-person" viewBox="0 0 16 16">
   // <path
   //     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
   //     fill="${cor}" /></svg>`
-  
+
   const idImg = document.createElement("label")
+  idImg.htmlFor = cor
   idImg.classList.add("img-jogador")
+  
   idImg.appendChild(svg)
- 
+
 
   return idImg
 }
@@ -77,8 +80,8 @@ btnCria.addEventListener("submit", (event) => {
 
   dadosSala.criarJogadorPlayer(identificador, namePlayer)
     .then(id => {
-     console.log(id)
-     localStorage.setItem("idPlayer", id.idPlayer)
+      console.log(id)
+      localStorage.setItem("idPlayer", id.idPlayer)
       if (identificador != false) {
 
         if (localStorage.getItem("flag") == null) {
@@ -87,14 +90,14 @@ btnCria.addEventListener("submit", (event) => {
         const MensagemErro = document.querySelector("[qual-erro]")
         MensagemErro.textContent = id.erro
         MensagemErro.style.display = "block"
-        console.log(id.erro) 
-        return 
+        console.log(id.erro)
+        return
         window.location.href = '../jogo/game.html'
-        
-              
+
+
       }
 
-  })
+    })
 
 })
 
